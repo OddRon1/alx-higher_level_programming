93bb20f3cc383524749d92734408b8db9cae9eee
Thamsanqa <ronduze@gmail.com>
2023-08-28 14:43:37 -0700

    0x04-python-more_data_structures

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """This script replaces or add skey/value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
